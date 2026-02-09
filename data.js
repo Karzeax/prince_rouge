e22e841acc09535f49fb1114e32dd89a3d6e73dc
Karzeax <xav.marieluce@gmail.com>
2026-02-09 11:46:41 +0100

    Fix image tag formatting in history entries

--- data.js
+++ data.js
@@ -11526,9 +11526,9 @@ const TURNS = [
     ],
     history: [
       { time: "09h01", text: '<img alt="Karz" src="https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/day/Karz.gif"> [CIC] Karz active son tour', result: "+12 PV" },
-      { time: "09h01", text: 'img alt="Monstre 29" src="https://www.kigard.fr/images/vue/monstre/29.gif"> Tréant active son tour', result: "" },
+      { time: "09h01", text: '<img alt="Monstre 29" src="https://www.kigard.fr/images/vue/monstre/29.gif"> Tréant active son tour', result: "" },
       { time: "09h02", text: '<img alt="Eressëa Sirthaal" src="https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/day/Eressëa%20Sirthaal.gif"> Eressëa Sirthaal active son tour', result: "" },
-      { time: "09h02", text: 'img alt="Monstre 29" src="https://www.kigard.fr/images/vue/monstre/29.gif"> Tréant se déplace', result: "" },
+      { time: "09h02", text: '<img alt="Monstre 29" src="https://www.kigard.fr/images/vue/monstre/29.gif"> Tréant se déplace', result: "" },
       {
         time: "09h02",
         text: '<img alt="Monstre 29" src="https://www.kigard.fr/images/vue/monstre/29.gif"> Tréant utilise Attaque puissante sur <img alt="Monstre 83" src="https://www.kigard.fr/images/vue/monstre/83.gif"> Prince Rouge',
